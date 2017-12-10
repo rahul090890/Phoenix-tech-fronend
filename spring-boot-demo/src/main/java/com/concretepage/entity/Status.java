824227110375b445beeada2e52a3702448e6cbe1
Sreenath <212636487@ge.com>
2017-12-10 19:18:53 +0530

    modified:   spring-boot-demo/src/main/java/com/concretepage/controller/CustomerProgramController.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/controller/EmployeeLeaveController.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/dao/ICustomerProgramDao.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/dao/IEmployeeLeaveBalanceDao.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/dao/impl/CustomerDAOImpl.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/dao/impl/CustomerProgramDaoImpl.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/dao/impl/DepartmentDAOImpl.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/dao/impl/EmployeeLeaveBalanceDao.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/dao/impl/ProjectDAOImpl.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/dao/impl/RoleDAOImpl.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/dao/impl/TaskDAOImpl.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/entity/Customer.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/entity/CustomerProgram.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/entity/Department.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/entity/EmployeeLeave.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/entity/EmployeeLeaveBalance.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/entity/LeaveMaster.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/entity/Project.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/entity/Role.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/entity/Task.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/service/EmployeeService.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/service/ICustomerProgramService.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/service/ILeaveService.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/service/impl/CustomerProgramServiceImpl.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/service/impl/LeaveService.java 	modified:   spring-boot-demo/src/main/java/com/concretepage/utils/HRUtils.java 	modified:   spring-boot-demo/src/main/resources/SQlChanges.txt

---
+++ spring-boot-demo/src/main/java/com/concretepage/entity/Status.java
@@ -0,0 +1,16 @@
+package com.concretepage.entity;
+
+public enum Status {
+	Active("Y"),
+	Inactive("N");
+	
+	private final String statusValue;
+	
+	private Status(String status) {
+		this.statusValue = status;
+	}
+	public String toString() {
+		return this.statusValue;
+	}
+	
+}
